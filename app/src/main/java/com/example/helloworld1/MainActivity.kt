51058ebb13f001523923584627f90f5f2f6eadd3
Stherove <prisnie11@gmail.com>
2025-08-21 04:10:37 -0400

    Adding the version with the two other buttons and an image.

--- app/src/main/java/com/example/helloworld1/MainActivity.kt
+++ app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+//import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,12 +18,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button).setOnClickListener{
             Log.i("Esther", "Tapped on button")
-            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.yellow))
+            findViewById<TextView>(R.id.textView).setTextColor(resources.getColor(R.color.yellow))
+
+        }
+        findViewById<Button>(R.id.button3).setOnClickListener{
+
+            Log.i("Esther", "Tapped on button")
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.red))
 
         }
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.backgroundView)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
