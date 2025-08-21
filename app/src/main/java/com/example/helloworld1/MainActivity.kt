9d15f3cb181cf1544762e92b0bd408449eb8b40e
Stherove <prisnie11@gmail.com>
2025-08-21 01:52:48 -0400

    First commit for the prework

---
+++ app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.helloworld1
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener{
+            Log.i("Esther", "Tapped on button")
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.yellow))
+
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
